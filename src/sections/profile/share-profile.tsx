839e1440fe9370f044b5bc13441cf7c2ce55799d
Constantin Ginga <41089075+constantinginga@users.noreply.github.com>
2023-11-26 15:25:11 +0100

    Remove clipboard functionality

--- src/sections/profile/share-profile.tsx
+++ src/sections/profile/share-profile.tsx
@@ -1,21 +1,8 @@
-import { toast } from 'react-hot-toast';
-
 const ShareProfile = ({ memberId }: { memberId: number }) => {
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(
-      `http://localhost:5173/external-profile/${memberId}`
-    );
-    toast.success('Profile URL copied to clipboard.', {
-      duration: 3000,
-    });
-  };
-
   return (
     <section className="flex flex-col gap-4 flex-1">
-      <h2 className="font-bold text-lg">Share external profile</h2>
-      <div
-        className="input input-bordered max-w-md w-full cursor-copy flex items-center"
-        onClick={copyToClipboard}>
+      <h2 className="font-bold text-lg">External profile link</h2>
+      <div className="input input-bordered max-w-md w-full flex items-center">
         <span>http://localhost:5173/external-profile/{memberId}</span>
       </div>
     </section>
